ce632f9d9e79ee7bc10d201d99a2e1e6e063ae15
fasyjsmn <78517875+fasyjsmn@users.noreply.github.com>
2021-02-18 09:25:19 +0700

    Update restaurantlist.php

--- restaurantlist.php
+++ restaurantlist.php
@@ -20,6 +20,26 @@
                             <td>Menara Standard Charter</td>
                             <td>03-12345678</td>
                         </tr>
+                          <?php
+                              //to link the connection to the database first
+                              $serverName = "simpledbtp057644.database.windows.net";
+                               $connectionOptions = array(
+                               "Database" => "simpledbtp057644",
+                               "Uid" => "fasyjsmn",
+                               "PWD" => "Fassya@1406");
+
+                              //Establishes the connection
+                               $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+                              if (!$conn)
+                              {
+                               die("Error connection: ".sqlsrv_errors());
+                               } 
+                               else
+                               {
+                                 echo ("connection done!");
+                               }
+                          ?>
                     </table>
             </center>
       </body>
