d3cf0a8d54e64367927ad47164ce44f2a19b65c5
fasyjsmn <78517875+fasyjsmn@users.noreply.github.com>
2021-02-18 09:09:14 +0700

    Create restaurantlist.php

---
+++ restaurantlist.php
@@ -0,0 +1,26 @@
+<html>
+      <head>
+            <title>List of restaurant in Bukit Jalil</title>
+      </head>
+      <body>
+           <center>
+                    <h1>List of restaurant in Bukit Jalil</h1>
+                    <br />
+                    <br />
+                    <table border="1">
+                        <tr>
+                              <th>Restaurant ID</th>
+                              <th>Name of Restaurant</th>
+                              <th>Addres of Restaurant</th>
+                              <th>Phone of Restaurant</th>
+                        </tr>
+                        <tr>
+                            <td>0</td>  <!--normal column-->
+                            <td>Subway</td>
+                            <td>Menara Standard Charter</td>
+                            <td>03-12345678</td>
+                        </tr>
+                    </table>
+            </center>
+      </body>
+</html>
